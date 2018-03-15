4c58a34aa51c58412357a35ac08bb8bc94f6710c
Dan Whettam <dwhettam@gmail.com>
2018-03-15 22:55:03 +0000

    Improved plotting + added grid searching hyperparameters

--- MLP.py
+++ MLP.py
@@ -2,18 +2,21 @@ from __future__ import print_function
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 import keras
+from keras.metrics import binary_accuracy
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
+from keras import backend as K
 from keras.optimizers import SGD
 import matplotlib.pyplot as plt
 import time
 
 start_time = time.time()
 batch_size = 128
-epochs = 100
+epochs = 1000
 
 # the data, shuffled and split between train and test sets
 df = (pd.read_csv('data.csv'))
@@ -24,36 +27,50 @@ y_train = train.iloc[:,-1]
 x_test = test.iloc[:,0:2]
 y_test = test.iloc[:,-1]
 
-
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
 # for idx, value in enumerate(df.ix[:,-1]):
 #     if value == 1:
 #         marker = "o"
+#         col = 'r'
 #     else:
 #         marker = "x"
-#     plt.scatter(df.ix[idx,0], df.ix[idx,1],c = 'b', marker = marker)
+#         col = 'b'
+#     plt.scatter(df.ix[idx,0], df.ix[idx,1],c = col, marker = marker)
 # plt.show()
 
-model = Sequential()
-model.add(Dense(124, activation='relu', input_shape = (2,)))
-model.add(Dropout(0.5))
-model.add(Dense(124, activation='relu'))
-model.add(Dropout(0.1))
-model.add(Dense(1, activation='sigmoid'))
-model.summary()
+learning_rates = [0.0001,0.001,0.01,0.1,0.2,0.4,0.8, 1]
+train_accuracies = []
+test_accuracies = []
+for learn_rate in learning_rates:
+    model = Sequential()
+    model.add(Dense(18, activation='relu', input_shape = (2,)))
+    model.add(Dense(18, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    model.summary()
 
-model.compile(loss='binary_crossentropy',
-              optimizer=SGD(lr = 1, momentum = 0.03),
-              metrics=['acc'])
+    model.compile(loss='binary_crossentropy',
+                  optimizer=SGD(lr = learn_rate, momentum = 0.01),
+                  metrics=[binary_accuracy])
 
-history = model.fit(x_train, y_train,
-                    batch_size=batch_size,
-                    epochs=epochs,
-                    verbose=1,
-                    validation_data=(x_test, y_test))
-score = model.evaluate(x_test, y_test, verbose=0)
+    history = model.fit(x_train, y_train,
+                        batch_size=batch_size,
+                        epochs=epochs,
+                        verbose=1,
+                        validation_data=(x_test, y_test))
+    score = model.evaluate(x_test, y_test, verbose=0)
+    train_accuracies.append(history.history['binary_accuracy'][-1] * 100)
+    test_accuracies.append(history.history['val_binary_accuracy'][-1] * 100)
+
+
+train, = plt.plot(learning_rates, train_accuracies, label = 'Train')
+#test, = plt.plot(nodes, test_accuracies, label = 'Test')
+#plt.legend([test, train], ['Test', 'Train'])
+plt.xlabel("Learning Rate")
+plt.ylabel("Accuracy %")
+plt.xscale('log')
+plt.show()
 
 y_pred = model.predict(x_test)
 y_pred[y_pred <= 0.5] = 0
